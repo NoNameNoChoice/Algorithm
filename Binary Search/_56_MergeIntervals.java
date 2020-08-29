e22abbebfee12ceb8fb635aff6d2047108da81a8
YmImm <Se7enYoung.wym@gmail.com>
2020-08-28 21:33:53 -0400

    Merge Intervals - Given a collection of intervals, merge all overlapping intervals.

---
+++ Binary Search/_56_MergeIntervals.java
@@ -0,0 +1,43 @@
+
+/*
+ * @ClassName: _56_MergeIntervals
+ * @Description:
+ * @Author: Se7en
+ * @Date: 2020/8/28 20:02
+ * @Version: 1.0
+ */
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class _56_MergeIntervals {
+
+    public static int[][] merge(int[][] intervals) {
+        if (intervals == null || intervals.length <= 1) {
+            return intervals;
+        }
+
+        Arrays.sort(intervals, new Comparator<int[]>() {
+            public int compare(int[] o1, int [] o2) {
+                return o1[0] - o2[0];
+            }
+        });
+
+        List<int[]> result = new ArrayList<>();
+        int[] newInterval = intervals[0];
+        result.add(newInterval);
+
+        for (int[] interval : intervals) {
+            if (interval[0] <= newInterval[1]) {
+                newInterval[1] = Math.max(newInterval[1], interval[1]);
+            } else {
+                newInterval = interval;
+                result.add(newInterval);
+            }
+        }
+
+        return result.toArray(new int[result.size()][]);
+    }
+}
